f4e0fc97997176757dc641b1a390932d3e6254f3
Juanjo <juanjo_fdez@yahoo.com.mx>
2021-06-22 10:58:21 -0500

    Computes date range

--- UFO-level-1/static/index.js
+++ UFO-level-1/static/index.js
@@ -1,6 +1,21 @@
 console.log("Hi... the truth is out there, not in here")
 var tbody = d3.select("tbody");
 
+// Obtaining dates range
+
+// Obtaining dates array
+dates = [];
+for (var i = 0; i < data.length; i++) {
+  var currentDate = data[i].datetime;
+  dates.push(Date.parse(currentDate));
+}
+
+// Obtaining min and max date
+minDate = Math.min(...dates);
+maxDate = Math.max(...dates);
+
+console.log(minDate, maxDate);
+
 // For testing, app will actually take the user input
 dateFilter = "1/7/2010"
 
@@ -18,10 +33,10 @@ buttonFilter.on("click", filterClick);
 
 // Function that filters, decided to not make it in line to be ready for part 2
 function filterData(event) {
-  
+
   // Filtering by Date
   if (dateFilter === event.datetime) {
-    
+
     // Appending results
     var row = tbody.append("tr");
     Object.entries(event).forEach(([key, value]) => {
